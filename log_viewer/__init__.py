287d43066ed79c1a48f04d6ae29fc281f947e9b4
Jonathan Strong <jonathan.strong@gmail.com>
2015-12-08 09:34:42 -0500

    added swp to gitignore

--- log_viewer/__init__.py
+++ log_viewer/__init__.py
@@ -6,11 +6,23 @@ import cPickle as pickle
 import struct
 import logging
 import time
+import StringIO
+
+#log_format = '[%(time_since_last_any)s][%(asctime)s][%(levelname)s][%(filename)s:%(funcName)s:%(lineno)s] %(message)s'
+log_format = '[%(asctime)s][%(levelname)s][%(filename)s:%(funcName)s:%(lineno)s] %(message)s'
+
+ws = None
 
 class TCP(tornado.tcpserver.TCPServer):
-    #def __init__(self, ws, *args, **kwargs):
-    #    self.ws = ws
-    #    super(TCP, self).__init__(*args, **kwargs)
+    def __init__(self, *args, **kwargs):
+        super(TCP, self).__init__(*args, **kwargs)
+        self.logger = logging.getLogger(__name__)
+        self.buffer = StringIO.StringIO()
+        self.log_handler = logging.StreamHandler(self.buffer)
+        self.log_format = logging.Formatter(log_format)
+        self.log_handler.setFormatter(self.log_format)
+        self.logger.addHandler(self.log_handler)
+        
 
     def handle_stream(self, stream, address):
         self._stream = stream
@@ -36,8 +48,8 @@ class TCP(tornado.tcpserver.TCPServer):
     def _process(self):
         #print self.data
 
-        obj = logging.makeLogRecord(pickle.loads(self.data))
-        print time.time(), obj
+        record = logging.makeLogRecord(self.unpickle(self.data))
+        self.handle_log_record(record)
         self.slen = None
         self.data = ''
         self._stream.read_bytes(4, self._first)
@@ -46,15 +58,34 @@ class TCP(tornado.tcpserver.TCPServer):
         return pickle.loads(data)
 
     def handle_log_record(self, record):
-        print record
+        """
+        we "handle" the log record, sending it to our StringIO
+        buffer, which we pass on to the websocket and then erase
+        from buffer.
+        """
+        self.logger.handle(record)
+        record = self.buffer.getvalue()
+        self.buffer.truncate(0)
+        if ws:
+            ws.write_message(record)
+        else: 
+            print 'ws is None, record not sent: {}'.format(record)
+
 
 
 class WebSocketHandler(tornado.websocket.WebSocketHandler):
+    def __init__(self, *args, **kwargs):
+        super(WebSocketHandler, self).__init__(*args, **kwargs)
+        if ws is None:
+            print 'setting {} to global ws'.format(self)
+            ws = self
+
     def open(self, *args):
         print "New connection"
         self.write_message("Welcome!")
 
     def on_message(self, message):
+        print 'websocket received this: {}'.format(message)
         self.write_message(message)
 
     def on_close(self):
